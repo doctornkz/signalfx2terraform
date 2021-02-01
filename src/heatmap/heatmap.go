3ac99de5eb83cccbe510f582b4834bd5949fd277
Ruslan Rotaru <rotarur.social.apps@gmail.com>
2021-02-01 23:29:41 +0000

    fixed colors fixed axis

--- src/heatmap/heatmap.go
+++ src/heatmap/heatmap.go
@@ -1,53 +1,53 @@
 package heatmap
 
 import (
-	"github.com/doctornkz/signalfx2terraform/src/utils"
+   "github.com/doctornkz/signalfx2terraform/src/utils"
 
-	"github.com/hashicorp/hcl2/hcl"
-	"github.com/hashicorp/hcl2/hclwrite"
-	"github.com/signalfx/signalfx-go/chart"
+   "github.com/hashicorp/hcl2/hcl"
+   "github.com/hashicorp/hcl2/hclwrite"
+   "github.com/signalfx/signalfx-go/chart"
 
-	"github.com/zclconf/go-cty/cty"
+   "github.com/zclconf/go-cty/cty"
 )
 
 // Chart - function for generating heatmap chart
 func Chart(f *hclwrite.File, chart *chart.Chart) *hclwrite.Body {
 
-	// program_text wrapper
-	programText := utils.ProgramTextProc(chart.ProgramText)
+   // program_text wrapper
+   programText := utils.ProgramTextProc(chart.ProgramText)
 
-	// tags wrapper
-	tags := chart.Tags
-	if tags == nil {
-		tags = []string{}
-	}
+   // tags wrapper
+   tags := chart.Tags
+   if tags == nil {
+      tags = []string{}
+   }
 
-	// wrapper around label
-	label := utils.LabelProc(chart.Id)
+   // wrapper around label
+   label := utils.LabelProc(chart.Id)
 
-	rootBody := f.Body()
-	chartBlock := rootBody.AppendNewBlock("resource", []string{utils.Type[chart.Options.Type], label})
-	chartBody := chartBlock.Body()
-	chartBody.SetAttributeValue("name", cty.StringVal(chart.Name))
-	chartBody.SetAttributeValue("description", cty.StringVal(chart.Description))
-	chartBody.SetAttributeTraversal("program_text", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: programText,
-		},
-	})
-	chartBody.SetAttributeValue("unit_prefix", cty.StringVal(chart.Options.UnitPrefix))
-	chartBody.SetAttributeValue("max_delay", cty.NumberIntVal(utils.MaxDelayProc(chart)))
-	chartBody.SetAttributeValue("group_by", utils.GroupByProc(chart))
+   rootBody := f.Body()
+   chartBlock := rootBody.AppendNewBlock("resource", []string{utils.Type[chart.Options.Type], label})
+   chartBody := chartBlock.Body()
+   chartBody.SetAttributeValue("name", cty.StringVal(chart.Name))
+   chartBody.SetAttributeValue("description", cty.StringVal(chart.Description))
+   chartBody.SetAttributeTraversal("program_text", hcl.Traversal{
+      hcl.TraverseRoot{
+         Name: programText,
+      },
+   })
+   chartBody.SetAttributeValue("unit_prefix", cty.StringVal(chart.Options.UnitPrefix))
+   chartBody.SetAttributeValue("max_delay", cty.NumberIntVal(utils.MaxDelayProc(chart)))
+   chartBody.SetAttributeValue("group_by", utils.GroupByProc(chart))
 
-	if chart.Options.ColorBy == "Range" {
-		utils.ColorRangeProc(chart, chartBody)
-	} else {
-		utils.ColorScale2Proc(chart, chartBody, utils.HeatmapColor)
-	}
+   if chart.Options.ColorBy == "Range" {
+      utils.ColorRangeProc(chart, chartBody)
+   } else {
+      utils.ColorScale2Proc(chart, chartBody)
+   }
 
-	chartBody.SetAttributeValue("minimum_resolution", cty.NumberIntVal(utils.MinResolutionProc(chart)))
-	chartBody.SetAttributeValue("disable_sampling", utils.DisableSamplingProc(chart))
-	chartBody.SetAttributeValue("refresh_interval", cty.NumberIntVal(utils.RefreshIntervalProc(chart)))
-	chartBody.AppendNewline()
-	return chartBody
+   chartBody.SetAttributeValue("minimum_resolution", cty.NumberIntVal(utils.MinResolutionProc(chart)))
+   chartBody.SetAttributeValue("disable_sampling", utils.DisableSamplingProc(chart))
+   chartBody.SetAttributeValue("refresh_interval", cty.NumberIntVal(utils.RefreshIntervalProc(chart)))
+   chartBody.AppendNewline()
+   return chartBody
 }
